e8ffd8d95ecbf230fe8a64930852a0c59afe0f7b
Bhartendu Awasthi <bhartendu.awasthi@kas-services.com>
2023-06-25 09:37:06 +0530

    added abs functionality

---
+++ furniture_problem_abs.py
@@ -0,0 +1,49 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+# product data
+products, price = gp.multidict({"table": 80, "chair": 45})
+
+# resources data
+resources, availability = gp.multidict({"mahogany": 400, "labour": 450})
+
+# rescources required by each product (bills of material)
+bom = {
+    ("mahogany", "chair"): 5,
+    ("mahogany", "table"): 20,
+    ("labour", "chair"): 10,
+    ("labour", "table"): 15,
+}
+
+model = gp.Model("furniture")
+
+make = model.addVars(products, name="make")
+
+res = model.addConstrs(
+    gp.quicksum(bom[r, p] * make[p] for p in products) <= availability[r]
+    for r in resources
+)
+
+# =============================================================================
+# absolute value of table and chair should be <= 5
+# =============================================================================
+
+diff_raw_table_chair = model.addVar(lb=-GRB.INFINITY)
+diff_abs_table_chair = model.addVar()
+
+model.addConstr(diff_raw_table_chair == make["table"] - make["chair"])
+model.addConstr(diff_abs_table_chair == gp.abs_(diff_raw_table_chair))
+model.addConstr(diff_abs_table_chair <= 5)
+
+# =============================================================================
+
+objective_function = make.prod(price)
+
+model.setObjective(objective_function, sense=GRB.MAXIMIZE)
+
+model.optimize()
+
+# display solution
+for v in make:
+    if abs(make[v].X) > 0.001:
+        print(make[v].varName, make[v].X)
